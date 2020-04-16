9573bbaad2bf05469d9890a09c31cdcb72562317
Daniel Cooper <cooperdaniel5141@gmail.com>
2020-04-16 18:43:21 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -21,3 +21,11 @@ def output_array array
     counter += 1
   end
 end
+
+def output_array array
+  counter = 0
+  while counter < array.length do
+    puts array[counter].to_s
+    counter += 1
+  end
+end
